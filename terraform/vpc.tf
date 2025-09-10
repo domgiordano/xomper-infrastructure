4bd9d9bf9f4044e2c9a3f33445355624a69ca7cc
Dominick Giordano <dominickgiordano@Dominicks-MacBook-Pro.local>
2025-09-10 14:32:20 -0400

    Update to vpc

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "public_subnet_1" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "${var.aws_region}a"
+  availability_zone = "us-east-1a"  # Change as needed
 
   tags = {
     Name = "PublicSubnet1"
@@ -22,32 +22,32 @@ resource "aws_subnet" "public_subnet_1" {
 resource "aws_subnet" "public_subnet_2" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "${var.aws_region}b"
+  availability_zone = "us-east-1b"  # Change as needed
 
   tags = {
     Name = "PublicSubnet2"
   }
 }
 
-# resource "aws_subnet" "private_subnet_1" {
-#   vpc_id            = aws_vpc.vpc.id
-#   cidr_block        = "10.0.3.0/24"
-#   availability_zone = "${var.aws_region}c"
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1c"  # Change as needed
 
-#   tags = {
-#     Name = "PrivateSubnet1"
-#   }
-# }
+  tags = {
+    Name = "PrivateSubnet1"
+  }
+}
 
-# resource "aws_subnet" "private_subnet_2" {
-#   vpc_id            = aws_vpc.vpc.id
-#   cidr_block        = "10.0.4.0/24"
-#   availability_zone = "${var.aws_region}d" 
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1d"  # Change as needed
 
-#   tags = {
-#     Name = "PrivateSubnet2"
-#   }
-# }
+  tags = {
+    Name = "PrivateSubnet2"
+  }
+}
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
@@ -80,42 +80,76 @@ resource "aws_route_table_association" "public_subnet_2_association" {
   route_table_id = aws_route_table.public_route_table.id
 }
 
-# resource "aws_nat_gateway" "nat" {
-#   allocation_id = aws_eip.nat_eip.id
-#   subnet_id    = aws_subnet.public_subnet_1.id
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id    = aws_subnet.public_subnet_1.id
 
-#   tags = {
-#     Name = "${var.app_name}-ngw"
+  tags = {
+    Name = "${var.app_name}-ngw"
+  }
+}
+
+resource "aws_eip" "nat_eip" {
+  tags = {
+    Name = "${var.app_name}-nat-eip"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "${var.app_name}-priv-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_1_association" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_subnet_2_association" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_security_group" "lambda_sg" {
+  name   = "${var.app_name}-lambda-sg"
+  vpc_id = aws_vpc.vpc.id
+
+#   ingress {
+#     from_port   = 0
+#     to_port     = 65535
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
 #   }
-# }
+    ingress {
+        from_port       = 0
+        to_port         = 65535
+        protocol        = "tcp"
+        cidr_blocks     = ["10.0.0.0/16"]  # Replace with your VPC CIDR block
+    }
+    ingress {
+        from_port       = 0
+        to_port         = 65535
+        protocol        = "tcp"
+        self            = true  # Allow traffic from the same security group
+    }
 
-# resource "aws_eip" "nat_eip" {
-#   tags = {
-#     Name = "${var.app_name}-nat-eip"
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # All traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-# resource "aws_route_table" "private_route_table" {
-#   vpc_id = aws_vpc.vpc.id
-
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     nat_gateway_id = aws_nat_gateway.nat.id
-#   }
-
-#   tags = {
-#     Name = "${var.app_name}-priv-rt"
-#   }
-# }
-
-# resource "aws_route_table_association" "private_subnet_1_association" {
-#   subnet_id      = aws_subnet.private_subnet_1.id
-#   route_table_id = aws_route_table.private_route_table.id
-# }
-
-# resource "aws_route_table_association" "private_subnet_2_association" {
-#   subnet_id      = aws_subnet.private_subnet_2.id
-#   route_table_id = aws_route_table.private_route_table.id
-# }
-
+  tags = {
+    Name = "LambdaSecurityGroup"
+  }
+}
 
