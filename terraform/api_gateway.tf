c0fe88c23dba45986d1209e9ca1519b406296fea
Dominick Giordano <dominickgiordano@Dominicks-MacBook-Pro.local>
2025-09-11 11:15:20 -0400

    Big update

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -61,8 +61,9 @@ resource "aws_api_gateway_deployment" "api_deploy" {
    create_before_destroy = true
  }
   depends_on = [
-    aws_api_gateway_resource.player_data_resource,
-    module.get_player_data_endpoint
+    aws_api_gateway_resource.player_resource,
+    module.get_player_data_endpoint,
+    module.post_player_data_endpoint
   ]
 }
 
@@ -110,18 +111,18 @@ resource "aws_api_gateway_gateway_response" "api_server_error_response" {
 # /player-data
 #**********************
 
-resource "aws_api_gateway_resource" "player_data_resource" {
-  path_part   = "player-data"
+resource "aws_api_gateway_resource" "player_resource" {
+  path_part   = "player"
   parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
 }
 
-# GET /player-data
+# GET /player/data
 module "get_player_data_endpoint" {
   source                  = "./modules/api_gateway"
   rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
-  parent_resource_id      = aws_api_gateway_resource.player_data_resource.id
-  path_part               = aws_api_gateway_resource.player_data_resource.path_part
+  parent_resource_id      = aws_api_gateway_resource.player_resource.id
+  path_part               = "data"
   http_method             = "GET"
   allow_methods           = ["GET", "OPTIONS"]
   allow_headers           = local.api_allow_headers
@@ -132,14 +133,39 @@ module "get_player_data_endpoint" {
   authorizer_id           = aws_api_gateway_authorizer.lambda_authorizer.id
   standard_tags           = local.standard_tags
   allow_origin            = "*"
-  modify_api_resource     = false
 }
 
+# POST /player/data
+module "post_player_data_endpoint" {
+  source                  = "./modules/api_gateway"
+  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+  parent_resource_id      = module.get_player_data_endpoint.api_gateway_resource_id
+  modify_api_resource     = true
+  http_method             = "POST"
+  allow_methods           = ["POST"]
+  allow_headers           = local.api_allow_headers
+  integration_type        = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.update_player_data.invoke_arn
+  authorization           = "CUSTOM"
+  authorizer_id           = aws_api_gateway_authorizer.lambda_authorizer.id
+  standard_tags           = local.standard_tags
+  allow_origin            = "*"
+}
+
+# AWS Permissions - user table
+resource "aws_lambda_permission" "update_user_table_data_post_permission"{
+  statement_id  = "AllowPostUpdatePlayerDataTable"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.update_player_data.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/POST/${aws_api_gateway_resource.player_resource.path_part}/data"
+}
 resource "aws_lambda_permission" "get_user_table_data_post_permission"{
-  statement_id  = "AllowGetUpdateUserTable"
+  statement_id  = "AllowGetPlayerDataTable"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.get_player_data.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/GET/${aws_api_gateway_resource.player_data_resource.path_part}"
+  source_arn    = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/GET/${aws_api_gateway_resource.player_resource.path_part}/data"
 }
 
