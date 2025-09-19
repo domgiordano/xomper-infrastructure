6a8582d135154835998c154d74f18bf90570053e
Dominick Giordano <dominickgiordano@Dominicks-MacBook-Pro.local>
2025-09-19 16:49:49 -0400

    fix

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -95,7 +95,7 @@ resource "aws_api_gateway_resource" "login_resource" {
 module "get_login_data_endpoint" {
   source                  = "./modules/api_gateway"
   rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
-  parent_resource_id      = aws_api_gateway_resource.api_gateway.id
+  parent_resource_id      = aws_api_gateway_resource.login_resource.id
   path_part               = "data"
   modify_api_resource     = false
   http_method             = "GET"
