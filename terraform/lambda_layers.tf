ef8d60ac6d55631da21db4794364625a8ea59b66
Dominick Giordano <dominickgiordano@Dominicks-MacBook-Pro.local>
2026-02-11 09:29:25 -0500

    didnt save .. again

--- terraform/lambda_layers.tf
+++ terraform/lambda_layers.tf
@@ -1,18 +1,18 @@
-## LAMBDA LAYER RESOURCES
+# LAMBDA LAYER RESOURCES
 
-# resource "aws_lambda_layer_version" "lambda_layer" {
-#   description = "Currently managed by ${var.app_name}"
-#   layer_name  = "${var.app_name}-shared-packages"
+resource "aws_lambda_layer_version" "lambda_layer" {
+  description = "Currently managed by ${var.app_name}"
+  layer_name  = "${var.app_name}-shared-packages"
 
-#   filename            = "./templates/lambda_stub.zip"
-#   source_code_hash    = filebase64sha256("./templates/lambda_stub.zip")
-#   compatible_runtimes = [var.lambda_runtime]
+  filename            = "./templates/lambda_stub.zip"
+  source_code_hash    = filebase64sha256("./templates/lambda_stub.zip")
+  compatible_runtimes = [var.lambda_runtime]
 
-#   lifecycle {
-#     ignore_changes = [
-#       description,
-#       filename,
-#       source_code_hash
-#     ]
-#   }
-# }
+  lifecycle {
+    ignore_changes = [
+      description,
+      filename,
+      source_code_hash
+    ]
+  }
+}
